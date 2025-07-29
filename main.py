9beb82170d51ddd46f3571e421d9ff500bcfc432
fortibit <robdomu@gmail.com>
2025-07-29 10:16:38 +0200

    Add screen display and close button functionality

--- main.py
+++ main.py
@@ -2,9 +2,28 @@ import pygame
 from constants import *
 
 def main():
+    pygame.init()
     print("Starting Asteroids!")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    fill_screen = pygame.Surface.fill(screen, (0, 0, 0))
+    refresh = pygame.display.flip()
+    
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        fill_screen
+        refresh
+
+
+def check_close_button():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return
+
 
 if __name__ == "__main__":
     main()
