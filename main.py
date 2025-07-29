1c6aa5381cf90fec7cf17faf0323e20adb69cdfc
fortibit <robdomu@gmail.com>
2025-07-29 12:55:29 +0200

    Refactor code

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0  # delta time
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0  # delta time
 
 
     while True:
@@ -16,10 +16,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill("black")
         player.update(dt)
+        screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
+
         #  limit framerate to 60 FPS and return frame time in seconds
         dt = clock.tick(60) / 1000 
 
