d3150707780d8448d7fb68b460ec3aae9e30b5cf
fortibit <robdomu@gmail.com>
2025-07-29 11:40:05 +0200

    Add circleshape.py class file and define player's ship size

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ def main():
 
         screen.fill("black")
         refresh
-        dt = clock.tick(60) / 1000 # return frame time in seconds
+        #  limit framerate to 60 FPS and return frame time in seconds
+        dt = clock.tick(60) / 1000 
 
 
 if __name__ == "__main__":
