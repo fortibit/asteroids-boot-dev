3ddf05a57addb6a42fdafa0d5ffbc03e5ed5eb33
fortibit <robdomu@gmail.com>
2025-07-29 09:53:35 +0200

    Add constanst and print screen size

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from asteroids-boot-dev!")
+import pygame
+from constants import *
 
+def main():
+    print("Starting Asteroids!")
+    print(f"Screen width: {constants.SCREEN_WIDTH}")
+    print(f"Screen height: {constants.SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
