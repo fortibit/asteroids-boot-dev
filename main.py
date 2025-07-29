24e9f712606834e5cea88102d89eceb0daddde67
fortibit <robdomu@gmail.com>
2025-07-29 11:18:01 +0200

    Add FPS limiter

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     refresh = pygame.display.flip()
+    clock = pygame.time.Clock()
+    dt = 0  # delta time
 
     while True:
         for event in pygame.event.get():
@@ -14,6 +16,7 @@ def main():
 
         screen.fill("black")
         refresh
+        dt = clock.tick(60) / 1000 # return frame time in seconds
 
 
 if __name__ == "__main__":
