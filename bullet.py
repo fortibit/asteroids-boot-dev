11912e4cc3899293dcf34e8311071d04be56305e
fortibit <robdomu@gmail.com>
2025-07-30 09:04:31 +0200

    Chore

--- bullet.py
+++ bullet.py
@@ -5,7 +5,6 @@ from constants import *
 class Bullet(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, BULLET_RADIUS)
-        #self.radius = SHOT_RADIUS
 
     def draw(self, screen):
         pygame.draw.circle(screen, "white", self.position, self.radius, 2)
